03d167f132636e8bf0eabc3d70663854ae53c6fa
EC2 Default User <ec2-user@ip-172-31-80-148.ec2.internal>
2022-12-12 20:45:22 +0000

    Adding skeleton

---
+++ skeleton/setup.py
@@ -0,0 +1,13 @@
+# --*--
+from setuptools import setup, find_packages
+
+setup(
+    name='skeleton',
+    version='0.1',
+    description='',
+    author='',
+    author_email='',
+    packages=find_packages(),
+    # setup_requires=['pytest-runner'],
+    # tests_require=['pytest'],
+)
