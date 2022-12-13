9cb200e3932180faafb1c8313935b5494d2fed94
EC2 Default User <ec2-user@ip-172-31-80-148.ec2.internal>
2022-12-13 16:02:52 +0000

    Adding some class method to Testing Class

--- skeleton/prodoction/test_utils.py
+++ skeleton/prodoction/test_utils.py
@@ -1,10 +1,47 @@
 import util # import module contains function to test
 
-def test_util():
+# First Test
+# def test_util():
     
-    # assert True
-    # logic testing
-    result = util.str_to_int('3.87')
-    assert result == 3
-# def some_test_utility():
-#     print("This is a utility")
+#     result = util.str_to_int('3.87')
+#     assert result == 3
+
+# Second Test
+# def test_float_rounds_down():
+    
+#     result = util.str_to_int('3.99')
+#     assert result == 3
+
+# Third Test
+# def test_float_round_down_lesser_half():
+    
+#     result = util.str_to_int('3.2')
+#     assert result == 3
+    
+# Convention Testing Using OOP
+
+class TestFloats:
+    
+    def setup(self):
+        print('\nthis is setup')
+        
+    def teardown(self):
+        print('\nthis is teardown')
+        
+    # Define some class methods
+    def setup_class(cls):
+        print('\nthis is setup class')
+    def teardown_class(cls):
+        print('\nthis is teardown class')
+    
+    # Second Test Inside Class
+    def test_rounds_down(self):
+
+        result = util.str_to_int('3.99')
+        assert result == 3
+        
+    # Third Test Inside Class
+    def test_round_down_lesser_half(self):
+    
+        result = util.str_to_int('3.2')
+        assert result == 3
