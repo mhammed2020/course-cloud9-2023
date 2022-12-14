6d6357ea146407c81b1ce44bf1b22ce3382b3315
EC2 Default User <ec2-user@ip-172-31-80-148.ec2.internal>
2022-12-14 12:56:10 +0000

    improving functional testing

---
+++ skeleton/03_pytest/response.py
@@ -0,0 +1,20 @@
+def build_message(response):
+    message = {
+        "success": True,
+        "error": "",
+    }
+    if response.status >= 400:
+        message["success"] = False
+        message["error"] = response.body
+    return message
+
+
+
+def test_build_message_success(response):
+    result = build_message(response())
+    assert result["success"] is True
+
+def test_build_message_failure(response):
+    result = build_message(response(400, "not allowed here!"))
+    assert result["success"] is False
+    assert result["error"] == "not allowed here!"
